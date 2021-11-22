479a12a27727fd35973edc17ae2331d1c90e49cd
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-11-22 15:40:17 -0600

    function that makes an album_object and adds it to the collection array //and returns the album_object and tests.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
+//function that makes an album_object and adds it to the collection array
+//and returns the album_object
 console.log('***** Music Collection *****')
+let collection = []
+function addToCollection(title, artist, year) {
+    let album={
+        title:title,
+        artist:artist,
+        year:year
+    }
+    collection.push(album)
+    return album
+}
+console.log(addToCollection("born again", "artist1", 1999));
+console.log(addToCollection("forever", "artist2", 1987));
+console.log(addToCollection("space jump", "artist1", 1999));
+console.log(addToCollection("torrent we live in", "artist3", 1999));
+console.log(addToCollection("alabama summer", "artist1", 1947));
+console.log(addToCollection("sanctioned", "artist2", 1988));
+console.log(collection);
+//End function that makes an album_object and adds it to the collection array
+//and returns the album_object and tests
+ 
+
