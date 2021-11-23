753b72e475ea4cf8fcaf9898b068a9b9c3cf2aff
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-11-23 16:16:37 -0600

    added function to show collection title by artist published in year

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,15 +1,15 @@
 //function that makes an album_object and adds it to the collection array
 //and returns the album_object
-console.log('***** Music Collection *****')
-let collection = []
+console.log("***** Music Collection *****");
+let collection = [];
 function addToCollection(title, artist, year) {
-    let album={
-        title:title,
-        artist:artist,
-        year:year
-    }
-    collection.push(album)
-    return album
+  let album = {
+    title: title,
+    artist: artist,
+    year: year,
+  };
+  collection.push(album);
+  return album;
 }
 console.log(addToCollection("born again", "artist1", 1999));
 console.log(addToCollection("forever", "artist2", 1987));
@@ -20,5 +20,14 @@ console.log(addToCollection("sanctioned", "artist2", 1988));
 console.log(collection);
 //End function that makes an album_object and adds it to the collection array
 //and returns the album_object and tests
- 
-
+//start function to show collection title by artist published in year
+function showCollection(array) {
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++) {
+    console.log(
+      `${array[i].title} by ${array[i].artist}, published in ${array[i].year}`
+    );
+  }
+}
+showCollection(collection);
+//end function to show collection title by artist published in year
