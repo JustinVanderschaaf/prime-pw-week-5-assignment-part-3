bf4a13cf12ea5a6d2c833c0693813bd616161018
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-11-28 14:36:51 -0600

    added tracks to addcollection and show collection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,31 +2,79 @@
 //and returns the album_object
 console.log("***** Music Collection *****");
 let collection = [];
-function addToCollection(title, artist, year) {
+function addToCollection(title, artist, year, tracks) {
   let album = {
     title: title,
     artist: artist,
     year: year,
+    tracks: tracks,
   };
   collection.push(album);
   return album;
 }
-console.log(addToCollection("born again", "artist1", 1999));
-console.log(addToCollection("forever", "artist2", 1987));
-console.log(addToCollection("space jump", "artist1", 1999));
-console.log(addToCollection("torrent we live in", "artist3", 1999));
-console.log(addToCollection("alabama summer", "artist1", 1947));
-console.log(addToCollection("sanctioned", "artist2", 1988));
+console.log(
+  addToCollection("born again", "artist1", 1999, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
+console.log(
+  addToCollection("forever", "artist2", 1987, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
+console.log(
+  addToCollection("space jump", "artist1", 1999, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
+console.log(
+  addToCollection("torrent we live in", "artist3", 1999, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
+console.log(
+  addToCollection("alabama summer", "artist1", 1947, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
+console.log(
+  addToCollection("sanctioned", "artist2", 1988, [
+    { name: "aaa", duration: "1:11" },
+    { name: "bbb", duration: "2:22" },
+    { name: "ccc", duration: "3:33" },
+  ])
+);
 console.log(collection);
 //End function that makes an album_object and adds it to the collection array
 //and returns the album_object and tests
 //start function to show collection title by artist published in year
 function showCollection(array) {
   console.log(array.length);
+  //loop through albums
   for (let i = 0; i < array.length; i++) {
+    //get the current album in the loop
+    let currentAlbum = array[i];
     console.log(
       `${array[i].title} by ${array[i].artist}, published in ${array[i].year}`
     );
+
+    //loop through the album's tracks
+    for (let j = 0; j < currentAlbum.tracks.length; j++) {
+      let currentTrack = currentAlbum.tracks[j];
+      if (currentTrack !== null) {
+        console.log(currentTrack.name + ": " + currentTrack.duration);
+      }
+    }
   }
 }
 showCollection(collection);
